43e67a3e85bd19f2e81139f15e3831aa0b2bfde2
Brandon McAnsh <brandon.mcansh@gmail.com>
2020-04-13 21:05:02 -0400

    chore: add GraphResult wrapper
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

---
+++ graphql/src/main/java/dev/bmcreations/graphql/model/GraphResult.kt
@@ -0,0 +1,33 @@
+package dev.bmcreations.graphql.model
+
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
+import retrofit2.HttpException
+import retrofit2.Response
+
+data class Extension(val code: String? = null)
+data class GraphQLError(val message: String, val extensions: Extension? = null)
+
+fun <T> GraphQLError.asException(): Throwable {
+    val responseCode = extensions?.code?.toIntOrNull() ?: 0
+
+    return if (responseCode > 0) {
+        // http exception
+        // create response for retrofit
+        val res = Response.error<T>(responseCode, ResponseBody.create(
+            "application/json".toMediaTypeOrNull(),
+            message
+        ))
+        HttpException(res)
+    } else {
+        GraphQLException(message)
+    }
+}
+
+abstract class GraphResult<T, E>(open val errors: List<GraphQLError>? = emptyList(), val data: T? = null) {
+    fun hasErrors(): Boolean = errors?.isNotEmpty() ?: false
+    abstract fun hasData(): Boolean
+    abstract fun result(): E?
+}
+
+class GraphQLException(override val message: String): Throwable(message)
